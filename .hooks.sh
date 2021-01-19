974cbfbcc357b270477d9708c6f05d301ca4fdad
Dan Steren <daniel.d.steren@gmail.com>
2021-01-19 00:48:37 -0700

    Add nvm

--- .hooks.sh
+++ .hooks.sh
@@ -13,6 +13,15 @@ if command -v rbenv > /dev/null 2>&1; then
   eval "$(rbenv init -)"
 fi
 
+# Load NVM
+if [ -d "$HOME/.nvm" ]; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+else
+  echo "WARN: nvm not installed. See https://github.com/nvm-sh/nvm#installing-and-updating"
+fi
+
 if command -v direnv > /dev/null 2>&1; then
   # Enable direnv. See https://direnv.net/
   # Make sure to create a .envrc file in any directories where this is used
